f5878ef44606d57481b2ce647a258585034446f0
luisveliz32 <56703377+luisveliz32@users.noreply.github.com>
2023-05-29 21:30:17 -0400

    Update objetoprectica.js

--- objetoprectica.js
+++ objetoprectica.js
@@ -1,11 +1,30 @@
+const persona =[{
+  
+  nombre: ['Bob', 'Smith'],
+  edad: 32,
+  genero: 'masculino',
+  intereses: ['música', 'esquí'],
+},
+{
+  
+  nombre: ['Maria', 'Carmen'],
+  edad: 31,
+  genero: 'Femenino',
+  intereses: ['música', 'Bailar'],
+}] ;
 const hola={"menu": {
-                    "id": "file",
-                    "value": "File",
-                    "popup": {
-                      "menuitem": [
-                        {"value": "New", "onclick": "CreateNewDoc()"},
-                        {"value": "Open", "onclick": "OpenDoc()"},
-                        {"value": "Close", "onclick": "CloseDoc()"}
-                      ]
-                    }
-                  }}
+  "id": "file",
+  "value": "File",
+  "popup": {
+    "menuitem": [
+      {"value": "New", "onclick": "CreateNewDoc()"},
+      {"value": "Open", "onclick": "OpenDoc()"},
+      {"value": "Close", "onclick": "CloseDoc()"}
+    ]
+  }
+}}
+console.log(hola.menu.id);
+console.log(hola.menu.value);
+ hola.menu.popup.menuitem.map(function(per) {
+  console.log( per.value+' '+ per.onclick);
+});
