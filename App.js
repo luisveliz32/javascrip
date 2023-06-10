8ea484f6bb564eae505f70f68b019fb2d9a4ce3b
luisveliz32 <56703377+luisveliz32@users.noreply.github.com>
2023-06-09 21:19:50 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import  React ,{useState,useEffect} from 'react';
+import { Button, View, Text,Image, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Principal from './components/Principal';
 
 function Principal({navigation}){
+  const[cont,setcont]=useState(0);
+  useEffect(()=>{
+    navigation.setOptions({
+      headerRight:()=>(
+        <Button 
+          onPress={()=>setcont(cont+1)}
+          title='contador'
+        />
+      )
+    })
+  })
   return(
+    
     <View style={{flex:1,alignItems:'center',justifyContent:'center'}} >
-      <Text>Pantalla Principal</Text>
+        <Text>Pantalla Principal{cont} </Text>
 
       <Button 
         title='Ir a la Siguiente pantalla'
@@ -15,23 +28,37 @@ function Principal({navigation}){
     </View>
   );
 }
-
-function Detalles(){
+function Logo(){
   return(
-    <View style={{flex:1,alignItems:'center',justifyContent:'center'}} >
-      <Text>Pantalla detalles</Text>
+    <Image
+        style={{width:50,height:50}}
+        source={{uri:'https://reactnative.dev/img/tiny_logo.png'}}
+    />
 
-    </View>
   )
-
 }
+
+
 const Stack=createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
         <Stack.Navigator initialRouteName='Principal'>
-          <Stack.Screen name='Principal' component={Principal}/>
-          <Stack.Screen name='Detalles' component={Detalles}/>
+          <Stack.Screen 
+              name='Principal' 
+              component={Principal}
+              options={{
+                headerTitle:(props)=><Logo{...props}/>,
+                headerRight:()=>(
+                  <Button
+                    onPress={()=>alert('presionaste aqi')}
+                    title='preciona aqi'
+                    color='#000'
+                  />
+                ),
+              }}
+          
+          />
         </Stack.Navigator> 
     </NavigationContainer>
   );
